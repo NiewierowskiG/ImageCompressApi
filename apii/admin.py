4d82418ceb9cce6e0c487345ce12eaff8f150de0
GrzegorzNiewierowski <gniewierowski@gmail.com>
2022-05-19 17:54:23 +0200

    admin

--- apii/admin.py
+++ apii/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import OriginalImage, User, Tier, CompressedImage
 
-# Register your models here.
+admin.site.register(OriginalImage)
+admin.site.register(User)
+admin.site.register(Tier)
+admin.site.register(CompressedImage)
+
