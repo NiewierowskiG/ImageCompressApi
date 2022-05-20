cc768c9fc97fbd62fcf302c6540e746420c3498a
CyDwaGy <czesiek2grzesiek@gmail.com>
2022-05-20 22:24:42 +0200

    Logic from views to another file

---
+++ apii/comress.py
@@ -0,0 +1,39 @@
+import sys
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from PIL import Image
+from .models import CompressedImage
+
+
+def check_tier_permissions(original_img, author, url):
+    tmp_context = {}
+    if author.tier.can_link_200px_height:
+        tmp_context['200px'] = create_thumbnail(200, original_img, url)
+    if author.tier.can_link_400px_height:
+        tmp_context['400px'] = create_thumbnail(400, original_img, url)
+    if author.tier.can_link_original_image:
+        tmp_context['original'] = f"{url}{original_img.img.url}"
+    if author.tier.can_link_custom_height and author.tier.custom_height_px > 0:
+        tmp_context['custom'] = create_thumbnail(author.tier.custom_height_px,
+                                             original_img, url)
+    return tmp_context
+
+
+def create_thumbnail(height, original_img, url):
+    img_tmp = Image.open(original_img.img.file.name)
+    width = int(height/img_tmp.height * img_tmp.width)
+    img_tmp.thumbnail((width, height), Image.ANTIALIAS)
+    output = BytesIO()
+    img_tmp.save(output, format="PNG", quality=75)
+    output.seek(0)
+    thumbnail = InMemoryUploadedFile(output, 'ImageField', original_img.img.name, 'image/png',
+                                     sys.getsizeof(output), None)
+    tmp = CompressedImage.objects.create(img=thumbnail, original=original_img, px=height)
+    return f"{url}{tmp.img.url}"
+
+"""
+def compress_image(original_img, px, url)
+    thumbnail200px = create_thumbnail(px, original_img.img.file.name, original_img.img.name)
+    tmp = CompressedImage.objects.create(img=thumbnail200px, original=original_img, px=px)
+    return f"{url}{tmp.img.url}"
+    """
--- apii/views.py
+++ apii/views.py
@@ -1,10 +1,4 @@
 import datetime
-import sys
-from io import BytesIO
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from PIL import Image
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from .serializer import OriginalImageSerializer
@@ -12,18 +6,7 @@ from .models import OriginalImage, User, CompressedImage, TemporaryUrl
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-
-
-def create_thumbnail(height, original_img, filename):
-    img_tmp = Image.open(original_img)
-    width = int(height/img_tmp.height * img_tmp.width)
-    img_tmp.thumbnail((width, height), Image.ANTIALIAS)
-    output = BytesIO()
-    img_tmp.save(output, format="PNG", quality=75)
-    output.seek(0)
-    thumbnail = InMemoryUploadedFile(output, 'ImageField', filename, 'image/png',
-                                     sys.getsizeof(output), None)
-    return thumbnail
+from .comress import create_thumbnail, check_tier_permissions
 
 
 class ImageViewSet(ListAPIView):
@@ -38,30 +21,14 @@ class ImageViewSet(ListAPIView):
         serializer = OriginalImageSerializer(data=request.data)
         print(self.request.user)
         if serializer.is_valid():
-            context = {}
             author = User.objects.get(user=request.user)
             original_img = OriginalImage.objects.create(img=request.data['img'], author=author)
-            if author.tier.can_link_200px_height:
-                thumbnail200px = create_thumbnail(200, original_img.img.file.name, original_img.img.name)
-                tmp = CompressedImage.objects.create(img=thumbnail200px, original=original_img, px=200)
-                context['200px'] = f"{request.get_host()}{tmp.img.url}"
-            if author.tier.can_link_200px_height:
-                thumbnail400px = create_thumbnail(400, original_img.img.file.name, original_img.img.name)
-                tmp = CompressedImage.objects.create(img=thumbnail400px, original=original_img, px=400)
-                context['400px'] = f"{request.get_host()}{tmp.img.url}"
-            if author.tier.can_link_original_image:
-                context['original'] = f"{request.get_host()}{original_img.img.url}"
-            if author.tier.can_link_custom_height and author.tier.custom_height_px > 0:
-                thumbnail_custom = create_thumbnail(author.tier.custom_height_px, original_img.img.file.name,
-                                                  original_img.img.name)
-                tmp = CompressedImage.objects.create(img=thumbnail_custom, original=original_img,
-                                                     px=author.tier.custom_height_px)
-                context['custom'] = f"{request.get_host()}{tmp.img.url}"
+            context = check_tier_permissions(original_img, author, request.get_host())
             return Response(context, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
 def get_temporary_url(request, url, time):
     expires = datetime.datetime.now() + datetime.timedelta(seconds=time)
-    eo = TemporaryUrl.objects.create(main_url=url, expires=expires)
+    TemporaryUrl.objects.create(main_url=url, expires=expires)
     return Response({"url": url, "time": time}, status=status.HTTP_200_OK)
